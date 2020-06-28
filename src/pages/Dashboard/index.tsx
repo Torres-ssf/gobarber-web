602d5bf2e6e8cc879b4f0ef8a5ad43647c8ee2a3
Torres <sergio.uft@gmail.com>
2020-06-27 21:11:36 -0300

    feat: add Dashboard files

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {} from './styles';
+import { Container } from '../SignIn/styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <h1>Dashboard</h1>
+    </Container>
+  );
+};
+
+export default Dashboard;
