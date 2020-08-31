333f5c9dd7b3635ca73611f32e6dab65e476e077
Torres-ssf <sergio.uft@gmail.com>
2020-08-31 20:03:35 -0300

    style: add avatar placeholder image to Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -30,6 +30,7 @@ import {
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
+import avatarPlaceholder from '../../assets/avatar-placeholder.png';
 
 interface MonthAvailabilityItem {
   day: number;
@@ -165,7 +166,10 @@ const Dashboard: React.FC = () => {
           <img src={logoImg} alt="GoBarber" />
 
           <Profile>
-            <img src={user.avatar_url} alt={user.name} />
+            <img
+              src={user.avatar_url ? user.avatar_url : avatarPlaceholder}
+              alt={user.name}
+            />
             <div>
               <span>Welcome,</span>
               <Link to="/profile">{user.name}</Link>
