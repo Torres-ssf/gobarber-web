5f6111f1f223bb743aa6fee1c6aa68c515cbdeb7
Torres-ssf <sergio.uft@gmail.com>
2021-02-25 15:00:41 -0300

    style(button component): add loading functionality

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,14 +1,14 @@
 import React, { ButtonHTMLAttributes } from 'react';
 
-import { Container } from './styles';
+import { Container, LoadingContainer } from './styles';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   loading?: boolean;
 };
 
 const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
-  <Container type="button" {...rest}>
-    {loading ? 'Loading' : children}
+  <Container type="button" loading={loading ? 1 : 0} {...rest}>
+    {loading ? <LoadingContainer /> : children}
   </Container>
 );
 
